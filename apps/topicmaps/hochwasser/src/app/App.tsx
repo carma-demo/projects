df82593d6ae1f86a321e44363bf36dcbc6a18472
Pavel <p-olhovoi@bk.ru>
2024-03-23 14:10:20 +0100

    update content

--- apps/topicmaps/hochwasser/src/app/App.tsx
+++ apps/topicmaps/hochwasser/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome hochwasser demo 👋
+        Welcome hochwasser demo updated 👋
       </h1>
     </div>
   );
