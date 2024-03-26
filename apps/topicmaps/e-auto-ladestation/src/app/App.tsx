01e1902f87d4d2ad9f8d8629cbe23327218d6c8f
Pavel <p-olhovoi@bk.ru>
2024-03-26 17:59:36 +0100

    skip node version

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation affected with nrwl/nx-set-shas@v4 base 2👋
+        Welcome e-auto-ladestation affected with nrwl/nx-set-shas@v4 base node👋
       </h1>
     </div>
   );
--- apps/topicmaps/hochwasser/src/app/App.tsx
+++ apps/topicmaps/hochwasser/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h2>
         <span> Hello there, </span>
-        Welcome to hochwasser affected with nrwl/nx-set-shas@v4 base 2👋
+        Welcome to hochwasser affected with nrwl/nx-set-shas@v4 base node👋
       </h2>
     </div>
   );
