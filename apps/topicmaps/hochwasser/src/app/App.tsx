52e9fdfbce551e84aa418f17d76a21fa122650ae
Pavel <p-olhovoi@bk.ru>
2024-03-27 11:55:18 +0100

    fix new loop

--- apps/topicmaps/hochwasser/src/app/App.tsx
+++ apps/topicmaps/hochwasser/src/app/App.tsx
@@ -7,7 +7,7 @@ export function App() {
       <h2>
         <span> Hello there, </span>
         Welcome to hochwasser affected with nrwl/nx-set-shas@v4 txt use outpu
-        tgh640 matrix 2 arr split 6 👋
+        tgh640 matrix 2 arr split 64 👋
       </h2>
     </div>
   );
