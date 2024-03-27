b48a7a4863073dcfbe92369dd64ccd9fd94dac52
Pavel <p-olhovoi@bk.ru>
2024-03-27 08:35:14 +0100

    add content

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation 6 👋
+        Welcome e-auto-ladestation 7 👋
       </h1>
     </div>
   );
