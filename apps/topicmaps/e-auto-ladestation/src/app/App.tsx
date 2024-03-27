2e0303a9deb08eb46b812b7c0c41619a81369a89
Pavel <p-olhovoi@bk.ru>
2024-03-27 18:43:58 +0100

    fix first tt

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -7,7 +7,7 @@ export function App() {
       <h1>
         <span> Hello there, </span>
         Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit
-        654dwssdwrsssw 👋
+        654dwssdwrsssw 👋s
       </h1>
     </div>
   );
