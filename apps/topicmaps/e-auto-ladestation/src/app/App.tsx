222f7701af5d11cf59bae4852464b1b733315346
Pavel <p-olhovoi@bk.ru>
2024-03-27 12:35:22 +0100

    advance splitting

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,8 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 65 👋
+        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 654
+        👋
       </h1>
     </div>
   );
