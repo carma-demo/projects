3eeb034964e4175d954039fa3b8eb60168e43068
Pavel <p-olhovoi@bk.ru>
2024-03-27 17:39:39 +0100

    fix sed

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,8 +6,8 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 654dw
-        👋
+        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit
+        654dws 👋
       </h1>
     </div>
   );
