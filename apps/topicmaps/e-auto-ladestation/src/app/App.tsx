1bed93ae6cf294a814ad5e3ed9428586e91c6086
Pavel <p-olhovoi@bk.ru>
2024-03-27 09:54:28 +0100

    fix id names

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt 👋
+        Welcome e-auto-ladestation txt 1 👋
       </h1>
     </div>
   );
--- apps/topicmaps/hochwasser/src/app/App.tsx
+++ apps/topicmaps/hochwasser/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h2>
         <span> Hello there, </span>
-        Welcome to hochwasser affected with nrwl/nx-set-shas@v4 txt 👋
+        Welcome to hochwasser affected with nrwl/nx-set-shas@v4 txt 1 👋
       </h2>
     </div>
   );
