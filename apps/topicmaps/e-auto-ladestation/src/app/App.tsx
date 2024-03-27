1ba776f3b59612562c104424d39f8e3904263100
Pavel <p-olhovoi@bk.ru>
2024-03-27 08:28:39 +0100

    add deploy helper

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation 55555 👋
+        Welcome e-auto-ladestation 6 👋
       </h1>
     </div>
   );
