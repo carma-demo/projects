6c171592af8a57848ed7b7fc730ce81c74b7d118
Pavel <p-olhovoi@bk.ru>
2024-03-27 11:26:24 +0100

    split array

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr 👋
+        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 👋
       </h1>
     </div>
   );
