7423792ab1648d6590d1724070c0b19b0457d566
Pavel <p-olhovoi@bk.ru>
2024-03-27 17:46:13 +0100

    add new sed pattern

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -7,7 +7,7 @@ export function App() {
       <h1>
         <span> Hello there, </span>
         Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit
-        654dwss 👋
+        654dwssw 👋
       </h1>
     </div>
   );
