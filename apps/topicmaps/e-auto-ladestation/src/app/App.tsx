6bdd02d7a02d5fabb3143a358c35103d7b027dc2
Pavel <p-olhovoi@bk.ru>
2024-03-27 11:51:44 +0100

    change loop

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 5 👋
+        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr slpit 6 👋
       </h1>
     </div>
   );
