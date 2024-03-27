ef2ff7b167eccede4e88bced78dba9f4c028c73b
Pavel <p-olhovoi@bk.ru>
2024-03-27 11:20:05 +0100

    show array

--- apps/topicmaps/e-auto-ladestation/src/app/App.tsx
+++ apps/topicmaps/e-auto-ladestation/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h1>
         <span> Hello there, </span>
-        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 👋
+        Welcome e-auto-ladestation txt use outpu tgh640 matrix 2 arr 👋
       </h1>
     </div>
   );
