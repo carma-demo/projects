41ace559bb7537b6737ce1e5f81a472438712bc9
Pavel <p-olhovoi@bk.ru>
2024-03-28 14:39:17 +0100

    skip npm install

--- apps/topicmaps/hochwasser/src/app/App.tsx
+++ apps/topicmaps/hochwasser/src/app/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div>
       <h2>
         <span> Hello there, </span>
-        Welcome to hochwasser demo 3.0!! 👋
+        Welcome to hochwasser demo 3.0!!! 👋
       </h2>
     </div>
   );
