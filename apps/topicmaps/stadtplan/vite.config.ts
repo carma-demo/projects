42d88278a2247a768cc03b2ed5039fee64f6742f
Pavel <p-olhovoi@bk.ru>
2024-04-02 11:40:42 +0200

    add stadtplan deploy config

--- apps/topicmaps/stadtplan/vite.config.ts
+++ apps/topicmaps/stadtplan/vite.config.ts
@@ -5,6 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
+  base: 'https://carma-demo.github.io/topic-stadtplan/',
   cacheDir: '../../../node_modules/.vite/apps/topicmaps/stadtplan',
 
   server: {
